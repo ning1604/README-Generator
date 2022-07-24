ac04c726118da7ec58510b0eb84f453f2686470a
Ning <ningzhawb@gmail.com>
2022-07-24 21:33:40 +1000

    added userPrompts and generateMarkdown functions

--- index.js
+++ index.js
@@ -1,13 +1,93 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const fs = require('fs')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the title of your project?",
+    "Please provide a short description of your project",
+    "What are the steps required to install your project?",
+    "Please provide instructions and examples for use?",
+    "What license will be used for your code?",
+    "Please provide contribution guidelines",
+    "Please provide explanation of testing procedures",
+    "Please provide GitHub username",
+    "Please provide preferred email address for contact purposes",
+];
+
+// Function to take in user input using inquirer prompted questions
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: questions[0],
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: questions[1],
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: questions[2],
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: questions[3],
+        },
+        // Following prompt uses inquirer list type
+        {
+            type: 'list',
+            name: 'licenseChoice',
+            message: questions[4],
+            choices: [
+                "MIT",
+                "GPLv2",
+                "GPLv3",
+                "Apache",
+                "BSD 2-clause",
+                "BSD 3-clause",
+                "LGPLv3",
+                "AGPLv3",
+                "MPL",
+                "EPL",
+                "Unlicense",
+            ]
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: questions[5],
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: questions[6],
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: questions[7],
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: questions[8],
+        },
+    ])
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(`${fileName}.md`, generateReadMe(data))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptUser()
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,36 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectTitle}
+![License: ${data.licenseChoice}]https://img.shields.io/badge/License-${licenseChoice}-green)
+## Description
+${data.description}
 
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Test](#test)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${data.licenseChoice}
+
+## Contributing
+${data.contributing}
+
+## Test
+${data.test}
+
+## Questions
+For further questions about the repository, open an issue or contact me directly at [${data.email}](mailto:${data.email}). You can find more of my work at [${data.username}](https://github.com/${data.username}?tab=repositories)
 `;
 }
 
